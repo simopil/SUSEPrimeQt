8a97026ac6c429cf8b927438c305ed18b8bcb9a0
simopil <painkilla96@gmail.com>
2019-02-18 02:45:27 +0100

    Little layout and translation adjustments - changed info button icon - less distance between widgets and reduced borders (smarter window) - removed useless items from translation file

--- README.md
+++ README.md
@@ -25,4 +25,7 @@ SUSEPrimeQt could work without root only for user-available features of SUSEPrim
 Code by simopil <a href="https://github.com/simopil">github</a>
 
 icons:
-Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a> 
+
+<a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+
+<a href="https://www.flaticon.com/authors/daniel-bruce" title="Daniel Bruce">Daniel Bruce</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
--- src/lang_ts/it_IT.ts
+++ src/lang_ts/it_IT.ts
@@ -4,267 +4,200 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.ui" line="35"/>
-        <source>SUSEPrimeQt</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="109"/>
+        <location filename="../mainwindow.ui" line="148"/>
         <source>Boot default</source>
         <translation>Default al boot</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="151"/>
-        <location filename="../mainwindow.ui" line="473"/>
+        <location filename="../mainwindow.ui" line="190"/>
+        <location filename="../mainwindow.ui" line="557"/>
         <source>Apply</source>
         <translation>Applica</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="194"/>
-        <source>N&amp;VIDIA</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="280"/>
+        <location filename="../mainwindow.ui" line="343"/>
         <source>&amp;LAST</source>
         <translation>&amp;Ultima utilizzata</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="323"/>
-        <source>&amp;INTEL</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="358"/>
+        <location filename="../mainwindow.ui" line="433"/>
         <source>Force next boot</source>
         <translation>Forza prossimo boot</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="436"/>
-        <location filename="../mainwindow.ui" line="1166"/>
+        <location filename="../mainwindow.ui" line="520"/>
+        <location filename="../mainwindow.ui" line="1150"/>
         <source>Restore</source>
         <translation>Ripristina</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="511"/>
-        <location filename="../mainwindow.ui" line="601"/>
-        <source>INTEL</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="516"/>
-        <source>INTEL2 [Opensource driver]</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="521"/>
-        <location filename="../mainwindow.ui" line="693"/>
-        <source>NVIDIA</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="396"/>
-        <source>Current: Not Set</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="237"/>
-        <source>INTEL 2 </source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="559"/>
+        <location filename="../mainwindow.ui" line="1419"/>
         <source>Switch</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="647"/>
-        <source>INTEL 2</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="728"/>
+        <location filename="../mainwindow.ui" line="652"/>
         <source>Nvidia card is</source>
         <translation>Stato Nvidia</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="780"/>
-        <source>OFF</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="816"/>
-        <source>[BBSWITCH]</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="180"/>
+        <location filename="../mainwindow.cpp" line="179"/>
         <source>BBSWITCH IS NOT INSTALLED</source>
         <translation>BBSWITCH NON È INSTALLATO</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="856"/>
+        <location filename="../mainwindow.ui" line="804"/>
         <source>Current driver:</source>
         <translation>Driver attuale:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="905"/>
-        <source>driver</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="945"/>
+        <location filename="../mainwindow.ui" line="902"/>
         <source>Settings</source>
         <translation>Impostazioni</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="974"/>
+        <location filename="../mainwindow.ui" line="931"/>
         <source>Console output</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1006"/>
+        <location filename="../mainwindow.ui" line="975"/>
         <source>Logfile</source>
         <translation>File di log</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1039"/>
+        <location filename="../mainwindow.ui" line="1011"/>
         <source>Logfile Clean</source>
         <translation>Pulisci log</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1093"/>
+        <location filename="../mainwindow.ui" line="1077"/>
         <source>Service</source>
         <translation>Servizio</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1244"/>
-        <source>DISABLED</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="1200"/>
+        <location filename="../mainwindow.ui" line="1184"/>
         <source>Status:</source>
         <translation>Stato:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1129"/>
+        <location filename="../mainwindow.ui" line="1113"/>
         <source>Disable</source>
         <translation>Disabilita</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1284"/>
+        <location filename="../mainwindow.ui" line="1274"/>
         <source>Advanced</source>
         <translation>Avanzate</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="1326"/>
+        <location filename="../mainwindow.ui" line="1316"/>
         <source>Reset settings</source>
         <translation>Imp. Default</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="43"/>
+        <location filename="../mainwindow.cpp" line="42"/>
         <source>logfile /var/log/prime-select.log NOT exist! Probably cleaned by user</source>
         <translation>Il file di log /var/log/prime-select.log Non esiste! È stato probabilmente pulito dall&apos;utente</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="74"/>
+        <location filename="../mainwindow.cpp" line="73"/>
         <source>intel2 driver already in use</source>
         <translation>Driver intel2 già in uso</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="75"/>
+        <location filename="../mainwindow.cpp" line="74"/>
         <source>intel driver already in use</source>
         <translation>Driver intel già in uso</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="76"/>
+        <location filename="../mainwindow.cpp" line="75"/>
         <source>nvidia driver already in use</source>
         <translation>Driver nvidia già in uso</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="78"/>
+        <location filename="../mainwindow.cpp" line="77"/>
         <source>&lt;font color=&quot;red&quot;&gt;NOT SET&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;red&quot;&gt;NON SETTATO&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="126"/>
+        <location filename="../mainwindow.cpp" line="125"/>
         <source>&lt;font color=&quot;red&quot;&gt;Current: INTEL2&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;red&quot;&gt;Attuale: INTEL2&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="127"/>
+        <location filename="../mainwindow.cpp" line="126"/>
         <source>&lt;font color=&quot;red&quot;&gt;Current: INTEL&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;red&quot;&gt;Attuale: INTEL&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="128"/>
+        <location filename="../mainwindow.cpp" line="127"/>
         <source>&lt;font color=&quot;red&quot;&gt;Current: NVIDIA&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;red&quot;&gt;Attuale: NVIDIA&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="131"/>
+        <location filename="../mainwindow.cpp" line="130"/>
         <source>&lt;font color=&quot;black&quot;&gt;Current: NOT SET&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;black&quot;&gt;Attuale: NON SETTATO&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="136"/>
+        <location filename="../mainwindow.cpp" line="135"/>
         <source>&lt;font color=&quot;green&quot;&gt;ENABLED&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;green&quot;&gt;ABILITATO&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="139"/>
+        <location filename="../mainwindow.cpp" line="138"/>
         <source>&lt;font color=&quot;red&quot;&gt;DISABLED&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;red&quot;&gt;DISABILITATO&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="149"/>
+        <location filename="../mainwindow.cpp" line="148"/>
         <source>Command line output:</source>
         <translation>Output comandi:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="159"/>
-        <location filename="../mainwindow.cpp" line="161"/>
+        <location filename="../mainwindow.cpp" line="158"/>
+        <location filename="../mainwindow.cpp" line="160"/>
         <source>package xf86-video-intel is not installed</source>
         <translation>Il pacchetto xf86-video-intel non è installato</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="175"/>
+        <location filename="../mainwindow.cpp" line="174"/>
         <source>Nvidia card will never powered off. This may cause overheating and low battery life. To have this feature, please install bbswitch package</source>
         <translation>La vga nvidia non verrà mai spenta, Può creare problemi di surriscaldamento e durata batteria. Per evitare ciò installa il pacchetto bbswitch</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="176"/>
+        <location filename="../mainwindow.cpp" line="175"/>
         <source>&lt;font color=&quot;red&quot;&gt;WARNING: BBSWITCH IS NOT INSTALLED&lt;/font&gt;</source>
         <translation>&lt;font color=&quot;red&quot;&gt;AVVISO: BBSWITCH NON È INSTALLATO&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="189"/>
+        <location filename="../mainwindow.cpp" line="188"/>
         <source>Set the default card at boot or remember last used</source>
         <translation>Imposta la scheda di default al boot o ricorda l&apos;ultima utilizzata</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="190"/>
+        <location filename="../mainwindow.cpp" line="189"/>
         <source>Select card only for next boot, default card setting will not change</source>
         <translation>Seleziona la scheda da utilizzare solamente al prossimo boot: la preferenza al boot rimarrà invariata</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="191"/>
+        <location filename="../mainwindow.cpp" line="190"/>
         <source>Manage the service. Remember SUSEPrime needs service enabled to correctly work</source>
         <translation>Gestisce il servizio. Ricorda che SUSEPrime necessita del servizio abilitato per funzionare correttamente</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="192"/>
+        <location filename="../mainwindow.cpp" line="191"/>
         <source>Disable service and reset configuration of SUSEPrime</source>
         <translation>Disabilita il servizio e porta a default la configurazione di SUSEPrime</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="193"/>
+        <location filename="../mainwindow.cpp" line="192"/>
         <source>Open nvidia-settings panel</source>
         <translation>Apre il pannello di controllo Nvidia</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="194"/>
+        <location filename="../mainwindow.cpp" line="193"/>
         <source>About and Informations</source>
         <translation>Informazioni</translation>
     </message>
@@ -277,33 +210,33 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../about.ui" line="64"/>
+        <location filename="../about.ui" line="67"/>
         <source>Coded by: simopil</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../about.ui" line="94"/>
+        <location filename="../about.ui" line="97"/>
         <source>&lt;a href=&quot;https://github.com/simopil/suseprimeqt&quot;&gt;Open on Github&lt;/a&gt;</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../about.ui" line="158"/>
+        <location filename="../about.ui" line="161"/>
         <source>SUSEPrimeqt  vers</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../about.ui" line="176"/>
+        <location filename="../about.ui" line="179"/>
         <source>Translated by:
 name</source>
         <translation>Tradotto da: simopil</translation>
     </message>
     <message>
-        <location filename="../about.ui" line="239"/>
+        <location filename="../about.ui" line="242"/>
         <source>DESCRIPTION+SUSEPRIME_VERSION_NEEDED string</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../about.ui" line="215"/>
+        <location filename="../about.ui" line="218"/>
         <source>Here a full command-line version usage and description</source>
         <translation>Descrizione e uso della versione shell</translation>
     </message>
